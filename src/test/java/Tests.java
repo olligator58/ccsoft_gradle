d384380649d739fee142b6c2cd552e069fbbbf40
Oleg <olligator58@gmail.com>
2025-11-26 08:47:09 +0300

    adding tests

---
+++ src/test/java/Tests.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Tests {
+
+    @Test
+    @DisplayName("Running tests of project...")
+    void testProject() {
+        assertEquals(1, 1);
+    }
+}
